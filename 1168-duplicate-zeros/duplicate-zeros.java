16f9e1f967f11ee109489e3b096fe100fc71fa70
ab7929332-gif <ab7929332@gmail.com>
2026-08-20 21:41:39 +0530

    Time: 1 ms (99.36%) | Memory: 46.9 MB (61.99%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int [] ans = new int[arr.length];
+        int s= 0;
+        int d = 0;
+        
+        while(s<arr.length)
+        {
+            if(arr[s]==0)
+            {
+                if(d<arr.length)
+                {
+                  ans[d]=0;
+                }
+                
+                d++;
+                if(d<arr.length)
+                {
+                ans[d]=0;
+                }
+                
+            }
+        
+            else 
+            {
+                if(d<arr.length)
+                {
+                ans[d] = arr[s];
+                }
+               
+            }
+             s++;
+             d++;
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=ans[i];
+        }
+    }
+}
